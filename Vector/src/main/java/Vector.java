8ad81b56d632e57384c4f752ffbfb1ba92f3d7d7
v.gargaun <gargaunveceslav@gmail.com>
2020-07-16 07:56:58 +0300

    commit

--- Vector/src/main/java/Vector.java
+++ Vector/src/main/java/Vector.java
@@ -1,16 +1,18 @@
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Setter
 @Getter
 @AllArgsConstructor
+
 public class Vector {
 
-    private double x1;
-    private double y1;
-    private double x2;
-    private double y2;
+    private double x1;  // Origine pe axa x
+    private double y1;  // Origine pe axa y
+    private double x2;  // Virful pe axa x
+    private double y2;  // Virful pe axa y
 
     public double lengthV()
     {
@@ -34,5 +36,30 @@ public class Vector {
         double y2 = vector2.getY2() - vector2.getY1()+vector1.getY2();
         Vector vector3 = new Vector(x1, y1, x2, y2);
         return vector3;
+    }
+
+    public double degreesVector(Vector vector) {
+        double degree = 0;
+        if(vector.x1<vector.x2 && vector.y1<vector.y2){
+            degree = degreesCalculation(vector.x1, vector.x2, vector.lengthV());
+
+        }
+
+        if(vector.x1 > vector.x2 && vector.y1<vector.y2){
+            degree = 180 - degreesCalculation(vector.x1, vector.x2, vector.lengthV());
+        }
+
+        if(vector.x1>vector.x2 && vector.y1>vector.y2){
+            degree = 180 + degreesCalculation(vector.x1, vector.x2, vector.lengthV());
+        }
+        if(vector.x1<vector.x2 && vector.y1>vector.y2){
+            degree = 360 - degreesCalculation(vector.x1, vector.x2, vector.lengthV());
+        }
+        return degree;
+    }
+
+    private double degreesCalculation (double x1, double x2, double length) {
+        double degrees = Math.toDegrees(Math.acos(Math.abs((x2-x1))/length));
+        return  degrees;
     }
 }
