b628335490184aefdb2af53c6f552622b96c7058
v.gargaun <gargaunveceslav@gmail.com>
2020-08-06 18:13:21 +0300

    second comit

---
+++ SpringProject/src/main/java/app/modele/Cars.java
@@ -0,0 +1,13 @@
+package app.modele;
+
+
+import lombok.Setter;
+
+@Setter
+
+public class Cars {
+    long id;
+    String model;
+    int price;
+
+}
---
+++ SpringProject/src/main/java/app/modele/Client.java
@@ -0,0 +1,8 @@
+package app.modele;
+
+public class Client {
+    long id;
+    String firstName;
+    String lastName;
+    int age;
+}
